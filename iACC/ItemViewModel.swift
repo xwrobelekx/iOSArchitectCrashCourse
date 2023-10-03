df72968d62930d8f99447b3b5f72e964aa6989e6
Kamil Wrobel <wrobelk@live.com>
2023-10-03 10:17:10 -0400

    Remove unneeded initalizer from ItemViewModel

--- iACC/ItemViewModel.swift
+++ iACC/ItemViewModel.swift
@@ -8,16 +8,4 @@ struct ItemViewModel {
     let title: String
     let subTitle: String
     let select: () -> Void
-    
-    init(_ item: Any, longDateStyle: Bool, selection: @escaping () -> Void) {
-        if let friend = item as? Friend {
-            self.init(friend: friend, selection: selection)
-        } else if let card = item as? Card {
-            self.init(card: card, selection: selection)
-        } else if let transfer = item as? Transfer {
-            self.init(transfer: transfer, longDateStyle: longDateStyle, selection: selection)
-        } else {
-            fatalError("unknown item: \(item)")
-        }
-    }
 }
